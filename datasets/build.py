36b04e90be8f22ba1b7c4f2f936d1dcb53c4e6bd
Eric Granger <etude.nguetche@gmail.com>
2025-04-24 15:09:46 -0400

    Keep the original batch_train, batch_val on the /datasets/build.py

--- datasets/build.py
+++ datasets/build.py
@@ -127,8 +127,7 @@ def build_train_dataloader(logger, config):
 
     train_loader = DataLoader(
         train_data, sampler=sampler_train,
-        #batch_size=config.batch_size,
-        batch_size=8,
+        batch_size=config.batch_size,
         num_workers=num_workers,
         pin_memory=pin_memory,
         drop_last=True,
@@ -193,8 +192,7 @@ def build_val_dataloader(logger, config, target_data_config):
 
     val_loader = DataLoader(
         val_data, sampler=sampler_val,
-        #batch_size=config.test_batch_size,
-        batch_size=8,
+        batch_size=config.test_batch_size,
         num_workers=num_workers,
         pin_memory=pin_memory,
         drop_last=False,
